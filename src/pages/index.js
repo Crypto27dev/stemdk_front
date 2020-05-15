f3d8611d4fdf2198185e81f6560d1f46ee90964b
AhmedHossam01 <ahmed.1318039@stemdakahlia.moe.edu.eg>
2020-05-15 22:46:24 +0200

    Applying i18n by sourcing content with JSON

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,29 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
+import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import { useStaticQuery, graphql } from "gatsby"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query EnglishStatements {
+      allDataJson(filter: { lang: { eq: "en" } }) {
+        edges {
+          node {
+            welcomingMessage
+          }
+        }
+      }
+    }
+  `).allDataJson.edges[0].node
+
+  console.log(data)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div>{data.welcomingMessage}</div>
+    </Layout>
+  )
+}
 
 export default IndexPage
